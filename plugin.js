44fc878ab3aae5f111605be8d5eff3172699678e
Havvy <ryan.havvy@gmail.com>
2017-09-20 21:30:10 -0700

    Initial functionality.

--- plugin.js
+++ plugin.js
@@ -1,12 +1,40 @@
-
+// Note(Havvy): This is a hacky way of doing it. A less hacky way would be
+//              to add in custom command detection to the Commands plugin.
 
 const InterpolateCommandPlugin = {
     name: "interpolate-command",
     requires: ["commands"],
 
     init (client, {commands}) {
+        const commandTrigger = client.config("command-trigger");
+        const firstInterpolateRegexp = new RegExp(`\\{(${commandTrigger}.*?)\\}`)
+
+        const findFirstInterpolate = function (message) {
+            const firstInterpolateMatch = message.match(firstInterpolateRegexp);
+
+            if (firstInterpolateMatch) {
+                return firstInterpolateMatch[1];
+            } else {
+                return undefined;
+            }
+        };
+
         return {
-        
+            handlers: {
+                privmsg: function (privmsg) {
+                    // If it's already a command, don't do anything.
+                    // Note(Havvy) If another plugin does something similar, well, this is hacky...
+                    if (commands.isCommand(privmsg)) { return; }
+
+                    const firstInterpolate = findFirstInterpolate(privmsg.message);
+                    if (!firstInterpolate) { return; }
+
+                    const psuedoPrivmsg = Object.create(privmsg);
+                    psuedoPrivmsg.message = firstInterpolate;
+
+                    return commands.handleCommand(psuedoPrivmsg, firstInterpolate.slice(commandTrigger.length));
+                }
+            }
         }
     }
 };
